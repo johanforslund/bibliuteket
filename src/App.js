39a38b246e8ce6114e8f5285c9264fb7c79a9999
adam Lindfors <adamlindfors@live.se>
2017-11-08 14:58:11 +0100

    added card component

--- src/App.js
+++ src/App.js
@@ -3,15 +3,26 @@ import {
   Text,
   View
 } from 'react-native';
+import Card from './components/Card';
 
 class App extends Component {
   render() {
     return (
-      <View>
-        <Text>Tom mall</Text>
+      <View style={styles}>
+        <Card>
+          <Text>Tom mall</Text>
+        </Card>
+        <Card>
+          <Text>Tom mall</Text>
+        </Card>
       </View>
     );
   }
 }
+
+const styles = {
+  backgroundColor: '#CFE3E9',
+  flex: 1
+};
 
 export default App;
